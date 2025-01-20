5a44616efaf68ab48b43d4037bb4942c5ac475ef
ppalacio <pedropalacioestrada@gmail.com>
2025-01-20 16:54:25 +0100

    icon

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Existing code...
-
-    // Check for reduced motion preference
     const prefersReducedMotion = window.matchMedia('(prefers-reduced-motion: reduce)').matches;
 
     if (!prefersReducedMotion) {
@@ -9,24 +6,6 @@ document.addEventListener('DOMContentLoaded', function() {
         AOS.init({
             duration: 1000,
             once: true,
-        });
-
-        // Initialize Ripple Effect
-        document.querySelectorAll('.buttons a').forEach(button => {
-            button.addEventListener('click', function (e) {
-                const ripple = document.createElement('span');
-                const diameter = Math.max(button.clientWidth, button.clientHeight);
-                const radius = diameter / 2;
-                ripple.style.width = ripple.style.height = `${diameter}px`;
-                ripple.style.left = `${e.clientX - button.offsetLeft - radius}px`;
-                ripple.style.top = `${e.clientY - button.offsetTop - radius}px`;
-                ripple.classList.add('ripple');
-                const rippleElement = button.getElementsByClassName('ripple')[0];
-                if (rippleElement) {
-                    rippleElement.remove();
-                }
-                button.appendChild(ripple);
-            });
         });
     }
 });
